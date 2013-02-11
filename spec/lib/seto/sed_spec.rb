be1b64f9e090d3c10724ab7665398cb8d4fbb5d2
kukenko <m.kukenko@gmail.com>
2013-02-11 14:37:40 +0900

    Use symbols for file paths

--- spec/lib/seto/sed_spec.rb
+++ spec/lib/seto/sed_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 module Seto
   describe Sed do
+    SIMPLE = './spec/files/simple.txt'
+    SETOIS = './spec/files/seto_is.txt'
+
+    let(:simple) { Sed.new(File.open(SIMPLE).each.with_index(1)) }
+    let(:setois) { Sed.new(File.open(SETOIS).each.with_index(1)) }
+
     it 'has the following methods' do
       m = Sed.new(File.open(__FILE__).each).methods
       m.should include(:edit)
@@ -12,37 +18,29 @@ module Seto
       m.should include(:s)
     end
 
-    describe '#address' do
-      it ''
-    end
-
     describe '#a' do
       it 'appends text to current line' do
-        sed = Sed.new(File.open('./spec/files/simple.txt').each.with_index(1))
-        sed.edit { a 'by ruby.'}
+        simple.edit { a 'by ruby.'}
         .should eql(["Seto is pseudo sed.\nby ruby."])
       end
     end
 
     describe '#d' do
       it 'deletes current line' do
-        sed = Sed.new(File.open(__FILE__).each.with_index(1))
-        sed.edit { d }.should be_empty
+        simple.edit { d }.should be_empty
       end
     end
 
     describe '#i' do
       it 'inserts text before current line' do
-        sed = Sed.new(File.open('./spec/files/simple.txt').each.with_index(1))
-        sed.edit { i 'by ruby.' }
+        simple.edit { i 'by ruby.' }
         .should eql(["by ruby.Seto is pseudo sed.\n"])
       end
     end
 
     describe '#s(pattern, replacement)' do
       it "substitutes 'replacement' for 'pattern'" do
-        sed = Sed.new(File.open('./spec/files/seto_is.txt').each.with_index(1))
-        sed.edit do
+        setois.edit do
           s /is/, 'no'
           s /a place name/, 'Hanayome'
           s /pseudo sed/, 'Hanayome'
